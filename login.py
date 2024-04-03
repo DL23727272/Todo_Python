3b098db526b92b1c9dc06a1f5fc187fe6ad98240
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-04-03 11:27:05 +0800

    removed an import on login.py

--- login.py
+++ login.py
@@ -2,7 +2,6 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
-from kivymd.uix.textfield import MDTextFieldRect
 from kivy.lang import Builder
 import subprocess
 import os
